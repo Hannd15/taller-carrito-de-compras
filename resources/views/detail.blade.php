8f5e7f2924185f7db1ed16aacb0a913dca28d05d
Esteban <egarcia93@unab.edu.co>
2024-04-03 22:49:15 -0500

    Trabajo orden de carrito de compras

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,23 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<h1 class="text-center m-5">Carrito</h1>
-<div class="container">
-@foreach ($cartItems as $cartItem)
-<form action="{{route('delete.store')}}" method="post">
-    @csrf
-    <div class="row">
-        <div class="col-md-8">
-            <img src="{{$cartItem->url_image}}" alt="{{$cartItem->name}}">
-        </div>
-        <div class="col-md-4 text-left">
-            <p>{{$cartItem->name}}</p>
-            <h3>${{$cartItem->price}}</h3>
-            <button type="submit" class="btn btn-primary" style="background-color: #f4860b;border-color: #f4860b">Añadir al carrito</button>
-        </div>
-    </div>
-</form>
-</div>
-@endforeach
 
+
+<body>
+    <div class="container">
+        <h1 class="text-center m-5">Carrito</h1>
+        @foreach ($cartItems as $cartItem)
+        <form action="{{route('delete.store')}}" method="post">
+            @csrf
+            <div class="row justify-content-center align-items-center mb-4">
+                <div class="col-md-8">
+                    <div class="border p-3 d-flex justify-content-center align-items-center"> 
+                        <div class="row">
+                            <div class="col-md-4 text-center">
+                                <img src="{{$cartItem->url_image}}" alt="{{$cartItem->name}}" style="max-width: 150px;">
+                            </div>
+                            <div class="col-md-6 text-left">
+                                <p>{{$cartItem->name}}</p>
+                                <h3>${{$cartItem->price}}</h3>
+                            </div>
+                            <div class="col-md-2 text-right">
+                                <button type="submit" class="btn btn-danger w-600 rounded" style="background-color: #f00;border-color: #f00;">Quitar del carrito</button> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+        @endforeach
+    </div>
+</body>
 @endsection
